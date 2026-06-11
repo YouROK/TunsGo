a77868506dce7fedbd3f9aad649e007170e7185c
YouROK <8yourok8@mail.ru>
2026-06-11 18:10:39 +0300

    add addrs

--- p2p/models/peer.go
+++ p2p/models/peer.go
@@ -4,6 +4,7 @@ import "time"
 
 type PeerInfo struct {
 	PeerID    string    `json:"peer_id"`
+	Addrs     []string  `json:"addrs,omitempty"`
 	Hosts     []string  `json:"hosts,omitempty"`
 	Timestamp int64     `json:"timestamp"`
 	LastResp  time.Time `json:"-"`
